ef3771155e93e557af693264d1b33639fe55ec82
Yi Yuan <fohn78@gmail.com>
2015-07-29 00:11:22 +0800

    add db-schema

--- fae/user/models.py
+++ fae/user/models.py
@@ -1,4 +1,14 @@
 __auther__ = 'fohnwind'
 
+
+from extensions import db
+
+
 class User(db.Model):
     __tablename__ = "users"
+    __searchable__ = ['username', 'email']
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(200), unique=True, nullable=False)
+    email = db.Column(db.String(200), unique=True, nullable=False)
+    _password = db.Column('password', db.String(120), nullable=False)
